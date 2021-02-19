4857eed56fc6643884d4fbba9381f6769916c681
thiagot3 <42971258+thiagot3@users.noreply.github.com>
2021-02-18 21:45:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+#importando o pandas.
 import pandas as pd
 
-dados = pd.read_csv('C:\\Users\\Thiago\\Desktop\\Transferencias.csv',
+# Lendo arquivo .CVS e salvando em um Pandas DataFrame
+dados = pd.read_csv('C:\\...\\arquivo.csv',
                         encoding='cp1252', sep=';',
                         engine='python', header=0)
 
+# Filtrando dados na variável dadosPB
 dadosPB = dados[(dados.UF == "PB")]
 
-print(dadosPB)
-
+# Exportando para um arquivo Exel
 dadosPB.to_excel(r'C:\\Users\\Thiago\\Desktop\\Transferencias.xlsx', index=False)
